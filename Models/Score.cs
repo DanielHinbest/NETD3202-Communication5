4d8a53df59c980a5ff2b280a40a62e246c3ed40b
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2020-12-12 21:36:21 -0500

    Add project files.

---
+++ Models/Score.cs
@@ -0,0 +1,41 @@
+﻿/* Daniel Hinbest
+ * NETD 3202 - Lab 5
+ * December 6, 2020
+ * This ASP.NET Core application is designed to allow the user to enter the information for a curling game, including the teams and the scores, and also provides functionality for editing, and deleting results.
+ * This model sets the content for the score record, with the scores for each team as well as how many ends each team won in a game
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETD3202_Communication5.Models
+{
+    public class Score
+    {
+        /// <summary>
+        /// The ID number of the score, also the primary key of the table
+        /// </summary>
+        public int scoreID { get; set; }
+
+        /// <summary>
+        /// The final score for the first team
+        /// </summary>
+        public int teamOneTotal { get; set; }
+
+        /// <summary>
+        /// The final score for the second team
+        /// </summary>
+        public int teamTwoTotal { get; set; }
+
+        /// <summary>
+        /// The number of ends the first team took in the game
+        /// </summary>
+        public int teamOneEnds { get; set; }
+
+        /// <summary>
+        /// The number of ends the second team took in the game
+        /// </summary>
+        public int teamTwoEnds { get; set; }
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,41 @@
+﻿/* Daniel Hinbest
+ * NETD 3202 - Lab 5
+ * December 6, 2020
+ * This ASP.NET Core application is designed to allow the user to enter the information for a curling game, including the teams and the scores, and also provides functionality for editing, and deleting results.
+ * This model sets the Team ID, as well as each of the players at each position
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETD3202_Communication5.Models
+{
+    public class Team
+    {
+        /// <summary>
+        /// The ID number for the table, also the primary key for the table
+        /// </summary>
+        public int teamID { set; get; }
+
+        /// <summary>
+        /// The lead's name. The lead is the player who throws first
+        /// </summary>
+        public string leadName { set; get; }
+
+        /// <summary>
+        /// The second's name. The second is the player who throws second
+        /// </summary>
+        public string secondName { set; get; }
+
+        /// <summary>
+        /// The vice's name. The vice throws third and takes over at the far end when the skip throws
+        /// </summary>
+        public string viceName { set; get; }
+
+        /// <summary>
+        /// The skip's name. The skip throws last and calls the game at the far end, essentially being the team captain.
+        /// </summary>
+        public string skipName { set; get; }
+    }
+}
